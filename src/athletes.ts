5a1e78872703b610b75cf5a1c0072c4c75530247
Alice <alice.mm.france@gmail.com>
2025-09-15 14:02:39 +0200

    test

---
+++ src/athletes.ts
@@ -0,0 +1,24 @@
+export interface Country {
+    code: string;
+    name: string;
+}
+export interface Nationality{
+    name: string;
+}
+
+export interface Physical_attributes{
+    weights: number;
+    height: number;
+}
+
+export interface Athlete {
+
+    code: number;
+    name: string;
+    gender: string;
+    nationality: Nationality;
+    country: Country;
+    disciplines: string[];
+    events: string[];
+    Physical_attributes: Physical_attributes
+}
