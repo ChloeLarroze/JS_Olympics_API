af3b01f1cd4d3c959410b752f4219f4debb78fad
Chloé <chloe.larroze@etu.emse.fr>
2025-09-22 18:17:36 +0200

    port fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 //import * as dotenv from 'dotenv';
 //dotenv.config();
 
-const PORT = 8080;
+const PORT = process.env.PORT ?? 3000;
 
 async function bootstrap() {
 
